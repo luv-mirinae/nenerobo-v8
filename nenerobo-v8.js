b79cf4224d804f9e5b0952ccf2feb0ce34133559
luv-mirinae <167957329+luv-mirinae@users.noreply.github.com>
2024-11-06 11:50:23 +0900

    Add StarLight codes

---
+++ nenerobo-v8.js
@@ -0,0 +1,14 @@
+/**
+ * @file Nenerobo Chatbot Main
+ * @author luv-mirinae
+ * @version 1.0.0-alpha
+ * @license AGPL-3.0
+ */
+
+'use strict';
+
+function onMessage(event) {}
+
+function onMessageDeleted(event) {}
+
+function onStartCompile() {}
